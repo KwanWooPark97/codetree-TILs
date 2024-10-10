26b9cc4be178224215ffa9e697e04543e11794a5
kwanwoopark <107847933+KwanWooPark97@users.noreply.github.com>
2024-10-10 16:54:12 +0900

    Title: 술래잡기, Time: 5078ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 241010/술래잡기/hide-and-seek.py
+++ 241010/술래잡기/hide-and-seek.py
@@ -2,6 +2,7 @@ import copy
 from collections import deque
 n,m,h,k=map(int,input().split())
 
+board=[[0]*n for _ in range(n)]
 path=[[0,0]]
 d=[0]
 d_2=[2]
@@ -27,6 +28,8 @@ for i in range(n*n):
 
     d.append(now_d)
     d_2.append((now_d+2)%4)
+move_board=[[0]*n for _ in range(n)]
+move_board_2=[[0]*n for _ in range(n)]
 
 
 d.append(2)
@@ -38,11 +41,22 @@ d=deque(d)
 d_2=deque(d_2)
 path_2=copy.deepcopy(path)
 path_2.reverse()
+for i in range(len(path)):
+    y,x=path[i]
+    move_board[y][x]=d[i]
+    y,x=path_2[i]
+    move_board_2[y][x]=d_2[i]
+
+
+
+
 class doduk():
     def __init__(self,y,x,dd):
         self.pos=[y,x]
         self.arrow=dd
         self.dead=False
+sulre=doduk(n//2,n//2,2)
+
 info=[]
 for i in range(m):
     y,x,dd=map(int,input().split())
@@ -52,11 +66,11 @@ for i in range(h):
     y,x=map(int,input().split())
     board[y-1][x-1]=1
 
-def move(now,info):
+def move(info,sulre):
     dx=[0,1,0,-1,0]
     dy=[0,0,1,0,-1]
     for i in range(len(info)):
-        dist=abs(now[0][0]-info[i].pos[0]) + abs(now[0][1]-info[i].pos[1])
+        dist=abs(sulre.pos[0]-info[i].pos[0]) + abs(sulre.pos[1]-info[i].pos[1])
         if not info[i].dead and dist<=3:
             new_y=info[i].pos[0]+dy[info[i].arrow]
             new_x=info[i].pos[1]+dx[info[i].arrow]
@@ -72,29 +86,29 @@ def move(now,info):
             elif new_x == -1:
                 info[i].arrow -= 2
                 new_x = info[i].pos[1] + dx[info[i].arrow]
-            if now[0]!=[new_y,new_x]:
+            if sulre.pos!=[new_y,new_x]:
                 info[i].pos=[new_y,new_x]
     return info
 
-def move_sulre(now,dd):
-    y,x=now.popleft()
-    dd.popleft()
-    if len(now)==0:
-        if y==0 and x==0:
-            now=copy.deepcopy(path)
-            dd=copy.deepcopy(d)
-
-        elif y==n//2 and x==n//2:
-            now = copy.deepcopy(path_2)
-            dd = copy.deepcopy(d_2)
-        now.popleft()
-        dd.popleft()
-    return now,dd
-def catch(board,now,dd,info):
+def move_sulre(sulre,now):
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
-    y,x=now[0]
-    arrow=dd[0]
+    new_y=sulre.pos[0]+dy[sulre.arrow]
+    new_x=sulre.pos[1]+dx[sulre.arrow]
+    if new_y==0 and new_x==0:
+        now=copy.deepcopy(move_board)
+    elif new_y==n//2 and new_x==n//2:
+        now=copy.deepcopy(move_board_2)
+    sulre.pos=[new_y,new_x]
+    sulre.arrow=now[new_y][new_x]
+
+    return sulre,now
+
+def catch(board,info,sulre):
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    y,x=sulre.pos
+    arrow=sulre.arrow
     cnt=0
     for i in range(3):
         new_y=y+(dy[arrow]*i)
@@ -105,23 +119,23 @@ def catch(board,now,dd,info):
                 info[j].dead=True
     return cnt
 
-now=copy.deepcopy(path_2)
-dd=copy.deepcopy(d_2)
 answer=0
+now=copy.deepcopy(move_board_2)
 for i in range(k):
-    info=move(now,info)
+    info=move(info,sulre)
     #print(info[0].pos)
-    now,dd=move_sulre(now,dd)
+    sulre,now=move_sulre(sulre,now)
     #print(now[0],dd[0])
-    cnt=catch(board, now, dd, info)
+    cnt=catch(board,info,sulre)
     answer+=cnt*(i+1)
     '''ret=copy.deepcopy(board)
     for j in range(len(info)):
         ret[info[j].pos[0]][info[j].pos[1]]=2
     ret[now[0][0]][now[0][1]]=3
     for j in range(n):
-        print(*ret[j])
 
+        print(*move_board_2[j])
+    print(sulre.pos)
     print("*"*20)'''
 print(answer)
 '''
